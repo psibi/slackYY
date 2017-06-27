b875308f9073027b1adbd1196388c900883b1704
Sibi Prabakaran <sibi@psibi.in>
2017-06-28 01:18:45 +0530

    Add App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import {
+  createStore,
+  applyMiddleware,
+} from 'redux';
 import { ChatHeader } from './components/chat/ChatHeader';
 import { ChatFooter } from './components/chat/ChatFooter';
 import MessageListings from './components/chat/MessageListings';
 import { MessageHistory } from './components/chat/MessageHistory';
+import rootReducer from './reducer';
+
+let middleWare = [thunkMiddleware];
+const loggerMiddleware = createLogger();
+middleWare = [...middleWare, loggerMiddleware];
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middleWare),
+);
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ChatHeader />
-        <div className="main">
-          <MessageListings />
-          <MessageHistory />
+      <Provider store={store}>
+        <div>
+          <ChatHeader />
+          <div className="main">
+            <MessageListings />
+            <MessageHistory />
+          </div>
+          <ChatFooter />
         </div>
-        <ChatFooter />
-      </div>
+      </Provider>
     );
   }
 }

dbca37b1252b395bcb0748c179da52530337d9ed
Sibi Prabakaran <sibi@psibi.in>
2017-06-28 01:19:33 +0530

    Add helper code

---
+++ client/src/helpers/utils.js
@@ -0,0 +1,25 @@
+import fetch from 'isomorphic-fetch';
+
+export function checkStatus(response) {
+  if (response.status === 200) {
+    return response;
+  }
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+
+export function fetchJSON(url, opts) {
+  const options = { ...opts };
+  options.headers = {
+    ...options.headers,
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+  if (('body' in options) && (typeof options.body !== 'string')) {
+    options.body = JSON.stringify(options.body);
+  }
+  return fetch(url, options)
+    .then(checkStatus)
+    .then(response => response.json());
+}

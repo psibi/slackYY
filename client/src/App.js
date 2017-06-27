bbd280caa677a5e6a1fadbbae4ab96ccc962e5a4
Sibi Prabakaran <sibi@psibi.in>
2017-06-27 18:13:19 +0530

    Fix warning

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { ChatHeader } from './components/chat/ChatHeader';
 import { ChatFooter } from './components/chat/ChatFooter';
 import { MessageListings } from './components/chat/MessageListings';

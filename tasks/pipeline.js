29044e9040a53ad736e5309327abee294bcee31b
Sibi Prabakaran <sibi@psibi.in>
2017-06-27 17:40:03 +0530

    Control js pipeline

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -29,11 +29,11 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  // 'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.js'
+  // 'js/**/*.js'
 ];
 
 

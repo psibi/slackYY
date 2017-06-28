9e104aa49b49f9d6d2c02389b6230e659a38a680
Sibi Prabakaran <sibi@psibi.in>
2017-06-29 00:38:04 +0530

    Have proper redirection to chat after successful login

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -26,10 +26,11 @@ module.exports = {
       }
       req.logIn(user, function(err) {
         if (err) res.send(err);
-        return res.send({
-          message: info.message,
-          user: user
-        });
+        /* return res.send({
+         *   message: info.message,
+         *   user: user
+         * });*/
+        return res.redirect('/chat');
       });
 
     })(req, res);

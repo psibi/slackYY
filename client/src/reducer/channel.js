30c37dbd99e495086d512da1f999476b44738b44
Sibi Prabakaran <sibi@psibi.in>
2017-06-28 11:52:25 +0530

    Handle UPDATE_CHANNEL

--- client/src/reducer/channel.js
+++ client/src/reducer/channel.js
@@ -1,4 +1,7 @@
-import { RECEIVE_CHANNEL } from '../actions/types';
+import 
+{ RECEIVE_CHANNEL,
+  UPDATE_CHANNEL,
+} from '../actions/types';
 
 const initialChannelState = {
   channelInfo: [],
@@ -10,6 +13,11 @@ function channel(state = initialChannelState, action) {
       return {
         ...state,
         channelInfo: action.data,
+      };
+    case UPDATE_CHANNEL:
+      return {
+        ...state,
+        channelInfo: [...state.channelInfo, action.data],
       };
     default:
       return state;

02e333b428d3cb49f15a4aef8ce722b82ad27112
Sibi Prabakaran <sibi@psibi.in>
2017-06-29 18:26:43 +0530

    Add logout link

--- client/src/components/chat/Navbar.jsx
+++ client/src/components/chat/Navbar.jsx
@@ -7,7 +7,8 @@ export function Navbar() {
         <div className="navbar-header"> <a href="javascript:void(0);" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a> <a href="javascript:void(0);" className="bars"></a> <a className="navbar-brand" href="index.html">SlackYY</a> </div>
         <div className="collapse navbar-collapse" id="navbar-collapse">
           <ul className="nav navbar-nav navbar-right">
-            <li className="pull-right"><a href="javascript:void(0);" className="js-right-sidebar" data-close="true">
+            <li className="pull-right">
+              <a href="/logout" className="js-right-sidebar" data-close="true">
 	      <i className="material-icons">power_settings_new</i>
 	    </a></li>
           </ul>

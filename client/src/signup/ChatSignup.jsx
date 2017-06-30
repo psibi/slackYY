f25e22d0650d0615535e6a25cf86185070adfe38
Sibi Prabakaran <sibi@psibi.in>
2017-06-30 16:04:15 +0530

    Fix ChatSignup

--- client/src/signup/ChatSignup.jsx
+++ client/src/signup/ChatSignup.jsx
@@ -15,25 +15,25 @@ export default class ChatSignup extends Component {
       password: '',
       confirmPassword: '',
       open: false,
-      snackMessage: ''
+      snackMessage: '',
     };
   }
 
   onStaticChange = field => (e) => {
     this.setState({ [field]: e.target.value });
   }
-  
+
   handleSubmit = (e) => {
     e.preventDefault();
-    const { name, email, password, confirmPassword } = {...this.state};
+    const { name, email, password, confirmPassword } = { ...this.state };
     if (password !== confirmPassword) {
       this.setState({
         open: true,
-        snackMessage: "Passwords don't match"
+        snackMessage: "Passwords don't match",
       });
       return;
     }
-    const requestData = { 
+    const requestData = {
       name,
       email,
       password,
@@ -44,120 +44,115 @@ export default class ChatSignup extends Component {
     })
       .then(checkStatus)
       .then(response => response.json())
-      .then((json) => {
-        window.location = "/login";
+      .then(() => {
+        window.location = '/login';
       })
       .catch((error) => {
         this.setState({
           open: true,
-          snackMessage: error.msg
+          snackMessage: error.msg,
         });
       });
   }
-  
-  onStaticFocus = field => (e) => {
-    if (field === 'name') {
-      this.textName.focus();
-    }
-    if (field === 'email') {
-      this.textEmail.focus();
-    }
-    if (field === 'password') {
-      this.textPassword.focus();
-    }
-    if (field === 'confirmPassword') {
-      this.textConfirmPassword.focus();
-    }
-  }
-  
+
   handleRequestClose = () => {
     this.setState({
       open: false,
     });
   };
 
-  render = () => {
-    return (
-      <div className="card">
-        <h4 className="l-login">Register
-          <div className="msg">Register a new membership</div>
-        </h4>
-        <form className="col-md-12" id="" onSubmit={this.handleSubmit}>
-          <div className="form-group form-float">
-            <div className={classNames('form-line', {focused: !_.isEmpty(this.state.name)})}>
-              <input 
-                type="text" 
-                className="form-control" 
-                value={this.state.name}
-                ref={(input) => { this.textName = input; }}
-                onClick={this.onStaticFocus('name')}
-                onChange={this.onStaticChange('name')}
-                name="name"/>
-              <label 
-                onClick={this.onStaticFocus('name')}
-                className="form-label">Name Surname</label>
-            </div>
+  render = () => (
+    <div className="card">
+      <h4 className="l-login">Register
+        <div className="msg">Register a new membership</div>
+      </h4>
+      <form className="col-md-12" id="" onSubmit={this.handleSubmit}>
+        <div className="form-group form-float">
+          <div className={classNames('form-line', { focused: !_.isEmpty(this.state.name) })}>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.name}
+              ref={(input) => { this.textName = input; }}
+              onChange={this.onStaticChange('name')}
+              name="name"
+              id="textName"
+            />
+            <label
+              htmlFor="textName"
+              className="form-label"
+            >Name Surname
+            </label>
           </div>
-          <div className="form-group form-float">
-            <div className={classNames('form-line', {focused: !_.isEmpty(this.state.email)})}>
-              <input 
-                type="email" 
-                className="form-control" 
-                value={this.state.email}
-                ref={(input) => { this.textEmail = input; }}
-                onClick={this.onStaticFocus('email')}
-                onChange={this.onStaticChange('email')}
-                name="email"/>
-              <label 
-                onClick={this.onStaticFocus('email')}
-                className="form-label">Email Address</label>
-            </div>
+        </div>
+        <div className="form-group form-float">
+          <div className={classNames('form-line', { focused: !_.isEmpty(this.state.email) })}>
+            <input
+              type="email"
+              className="form-control"
+              value={this.state.email}
+              ref={(input) => { this.textEmail = input; }}
+              onChange={this.onStaticChange('email')}
+              name="email"
+              id="textEmail"
+            />
+            <label
+              htmlFor="textEmail"
+              className="form-label"
+            >Email Address</label>
           </div>
-          <div className="form-group form-float">
-            <div className={classNames('form-line', {focused: !_.isEmpty(this.state.password)})}>
-              <input 
-                type="password" 
-                className="form-control"
-                value={this.state.password}
-                ref={(input) => { this.textPassword = input; }}
-                onClick={this.onStaticFocus('password')}
-                onChange={this.onStaticChange('password')}
-                name="password"/>
-              <label 
-                onClick={this.onStaticFocus('password')}
-                className="form-label">Password</label>
-            </div>
+        </div>
+        <div className="form-group form-float">
+          <div className={classNames('form-line', { focused: !_.isEmpty(this.state.password) })}>
+            <input
+              type="password"
+              className="form-control"
+              value={this.state.password}
+              ref={(input) => { this.textPassword = input; }}
+              onChange={this.onStaticChange('password')}
+              name="password"
+              id="textPassword"
+            />
+            <label
+              htmlFor="textPassword"
+              className="form-label"
+            >Password</label>
           </div>
-          <div className="form-group form-float">
-            <div className={classNames('form-line', {focused: !_.isEmpty(this.state.confirmPassword)})}>
-              <input 
-                type="password" 
-                value={this.state.confirmPassword}
-                ref={(input) => { this.textConfirmPassword = input; }}
-                onClick={this.onStaticFocus('confirmPassword')}
-                onChange={this.onStaticChange('confirmPassword')}
-                className="form-control"/>
-              <label 
-                onClick={this.onStaticFocus('confirmPassword')}
-                className="form-label">Confirm Password</label>
-            </div>
+        </div>
+        <div className="form-group form-float">
+          <div className={classNames('form-line', { focused: !_.isEmpty(this.state.confirmPassword) })}>
+            <input
+              type="password"
+              value={this.state.confirmPassword}
+              ref={(input) => { this.textConfirmPassword = input; }}
+              onChange={this.onStaticChange('confirmPassword')}
+              className="form-control"
+              id="textConfirmPassword"
+            />
+            <label
+              htmlFor="textConfirmPassword"
+              className="form-label"
+            >Confirm Password</label>
           </div>
-          <div className="text-left"> 
-            <a
-              onClick={this.handleSubmit}
-              href="#signup" className="btn btn-raised waves-effect bg-red" type="submit">SIGN UP</a> 
-          </div>
-          <div className="m-t-10 m-b--5"> <a href="/login">You already have a membership?</a> </div>
-        </form>
-        <MuiThemeProvider>
-          <Snackbar
-            open={this.state.open}
-            message={this.state.snackMessage}
-            autoHideDuration={4000}
-            onRequestClose={this.handleRequestClose}
-          />
-        </MuiThemeProvider>
-      </div>
-    );
-  }
+        </div>
+        <div className="text-left">
+          <a
+            onClick={this.handleSubmit}
+            href="#signup"
+            className="btn btn-raised waves-effect bg-red"
+            type="submit"
+          >SIGN UP</a>
+        </div>
+        <div className="m-t-10 m-b--5"> <a href="/login">You already have a membership?</a> </div>
+      </form>
+      <MuiThemeProvider>
+        <Snackbar
+          open={this.state.open}
+          message={this.state.snackMessage}
+          autoHideDuration={4000}
+          onRequestClose={this.handleRequestClose}
+        />
+      </MuiThemeProvider>
+    </div>
+  )
 }
